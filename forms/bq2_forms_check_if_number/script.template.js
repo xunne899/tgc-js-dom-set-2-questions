41a48864648551dba802785141acea57eb568928
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-05-26 06:45:56 +0000

    changes to forms qn2

--- forms/bq2_forms_check_if_number/script.template.js
+++ forms/bq2_forms_check_if_number/script.template.js
@@ -1,5 +1,12 @@
 // write your code here
+let = button.document.querySelector('process')
+button.addEventListener('click',function(){
 
+let num1
+let num2
+let num3
+
+})
 
 // --- LEAVE THE BELOW ALONE ----
 function isNumber(x) {
